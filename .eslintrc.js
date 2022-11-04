7d5b137af69a0278414c4b94e9a6648759bdfb27
rynxiao <yuzhongzi91@sina.com>
2022-11-04 16:29:35 +0800

    refactor: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,15 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'airbnb-base',
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:vue/vue3-essential', 'airbnb-base'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'vue',
-  ],
+  plugins: ['vue'],
   rules: {
+    semi: ['error', 'never'],
+    'import/no-extraneous-dependencies': ['off'],
   },
-};
+}
